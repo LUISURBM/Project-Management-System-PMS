a8f1d5f9fbfc5dde18c0ab8eccbb26850828973f
LUIS FELIPE URBINA MORENO <LUIZ-FELIPE16@HOTMAIL.COM>
2026-02-10 20:30:44 +0000

    clean logs

--- hotel-services/index.js
+++ hotel-services/index.js
@@ -1,22 +1,6 @@
 // Initializes Total.js framework 5
-
+console.log('INIT --> process.env.DATABASE_CONNECTION', process.env.DATABASE_CONNECTION);
 require('total5');
-
 const options = {};
-if (process.env.MYSQL_URL) {
-    process.env.database = process.env.DATABASE_CONNECTION;
-}
-options.release = true;
-options.directory = __dirname + '/../';
-options.config = {
-    database: process.env.DATABASE_CONNECTION || process.env.MYSQL_URL
-};
 Total.run(options);
-setTimeout(() => console.log('INIT ==> CONF.database', CONF.database), 2000);
-setTimeout(() => console.log('INIT --> process.env.DATABASE_CONNECTION', process.env.DATABASE_CONNECTION), 2000);
-// setTimeout(() => console.log('Mode (CONF.release):', CONF.release), 2000);
-// setTimeout(() => console.log('Mode (CONF.debug):', CONF.debug), 2000);
-// setTimeout(() => console.log('DEBUG -> process.env', process.env), 2000);
-// setTimeout(() => console.log('DEBUG -> process.env.MYSQL_URL', process.env.MYSQL_URL), 2000);
-// setTimeout(() => console.log('DEBUG -> process.env.NODE_ENV', process.env.NODE_ENV), 2000);
-// setTimeout(() => console.log('DEBUG -> process.env.RAILWAY_ENVIRONMENT_NAME', process.env.RAILWAY_ENVIRONMENT_NAME), 2000);
+console.log('INIT ==> CONF.database', CONF.database);
