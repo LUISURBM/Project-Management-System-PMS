28d75f5576dd7b0136e9992eb9b23e7b7aa0255d
LUIS FELIPE URBINA MORENO <LUIZ-FELIPE16@HOTMAIL.COM>
2026-02-10 03:38:29 +0000

    index clean

--- hotel-services/index.js
+++ hotel-services/index.js
@@ -2,40 +2,7 @@
 
 require('total5');
 
-
-// Launches a web server
-// Total.http({ load: 'none' });
-
 const options = {};
-
-// options.cluster = 5; // opens 5 threads
-// options.cluster = 'auto';
-// options.release = true;
-// options.release = process.env.RAILWAY_ENVIRONMENT_NAME === 'production' || process.argv.includes('--release');
-// Detectar Railway production
-
-// options.ip = '127.0.0.1';
-// options.port = parseInt(process.argv[2]);
-// options.unixsocket = PATH.join(F.tmpdir, 'app_name.socket');
-// options.unixsocket777 = true;
-// options.config = { name: 'Total.js' };
-// options.sleep = 3000;
-// options.inspector = 9229;
-// options.watch = ['private'];
-// options.livereload = 'https://yourhostname';
-// options.watcher = false; // disables watcher
-// options.edit = 'wss://www.yourcodeinstance.com/?id=projectname'
-
-// options.release = process.argv.includes('--release');
-
-// Service mode:
-// options.servicemode = process.argv.includes('--service') || process.argv.includes('--servicemode');
-// options.servicemode = 'definitions,modules,config';
-
-// Cluster:
-// options.tz = 'utc';
-// options.cluster = 'auto';
-// options.limit = 10; // max 10. threads (works only with "auto" scaling)
 
 Total.run(options);
 setTimeout(() => console.log('DEBUG -> process.env.DATABASE_CONNECTION', process.env.DATABASE_CONNECTION), 2000);
