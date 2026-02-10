313aadd63996901022edb5321ac43b73b866e8c9
LUIS FELIPE URBINA MORENO <LUIZ-FELIPE16@HOTMAIL.COM>
2026-02-10 03:19:08 +0000

    main release env prod variable railway

--- hotel-services/app/index.js
+++ hotel-services/app/index.js
@@ -8,8 +8,8 @@ require('total5');
 
 const options = {};
 
-options.cluster = 5; // opens 5 threads
-options.cluster = 'auto';
+// options.cluster = 5; // opens 5 threads
+// options.cluster = 'auto';
 options.release = true;
 // options.release = process.env.RAILWAY_ENVIRONMENT_NAME === 'production' || process.argv.includes('--release');
 // Detectar Railway production
@@ -37,9 +37,19 @@ options.release = true;
 // options.cluster = 'auto';
 // options.limit = 10; // max 10. threads (works only with "auto" scaling)
 
+options.config = { 
+    database: process.env.MySQL.MYSQL_URL,
+    name: 'PMS Hotel Services'
+};
 
 Total.run(options);
 setTimeout(() => console.log('DEBUG -> process.env.NODE_ENV', process.env.NODE_ENV), 2000);
 setTimeout(() => console.log('DEBUG -> process.env.RAILWAY_ENVIRONMENT_NAME', process.env.RAILWAY_ENVIRONMENT_NAME), 2000);
 setTimeout(() => console.log('DEBUG -> Mode:', CONF.release ? 'RELEASE' : 'DEBUG'), 2000);
 setTimeout(() => console.log('INIT ==> Current Database Config:', CONF.database), 2000);
+setTimeout(() => {
+    console.log('--- SYSTEM CHECK ---');
+    console.log('Flag (options.release):', options.release);
+    console.log('Database URL:', CONF.database); // If this shows your Railway URL, it worked!
+    console.log('Mode (CONF.debug):', CONF.debug); // Total 5 uses CONF.debug internally
+}, 2000);
