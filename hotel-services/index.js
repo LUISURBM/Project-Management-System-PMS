39e635953ec28e8afa762d62fc034982c121e706
LUIS FELIPE URBINA MORENO <LUIZ-FELIPE16@HOTMAIL.COM>
2026-02-10 20:43:37 +0000

    clean logs

--- hotel-services/index.js
+++ hotel-services/index.js
@@ -2,5 +2,14 @@
 console.log('INIT --> process.env.DATABASE_CONNECTION', process.env.DATABASE_CONNECTION);
 require('total5');
 const options = {};
+
+if (process.env.DATABASE_CONNECTION) {
+    process.env.database = process.env.DATABASE_CONNECTION;
+}
+options.release = true;
+options.directory = __dirname + '/../';
+options.config = {
+    database: process.env.DATABASE_CONNECTION || process.env.MYSQL_URL
+};
 Total.run(options);
 console.log('INIT ==> CONF.database', CONF.database);
